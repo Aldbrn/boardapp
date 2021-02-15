13fa580d99b6596a1ac82645ace193a2aed9a332
Aldbrn <aldbrn.xxx@gmail.com>
2021-02-16 01:53:14 +0900

    add new file

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 
 
 def signupfunc(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            User.objects.create_user(username, "", password)
+            return render(request, "signup.html", {})
+        except IntegrityError:
+            return render(
+                request, "signup.html", {"error": "This username is already taken."}
+            )
     return render(request, "signup.html", {})
+
+def loginfunc(request):
+    
