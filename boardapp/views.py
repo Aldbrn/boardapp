30fbe61dc3f4c9367859d21fc2c37fa1274662d1
Aldbrn <aldbrn.xxx@gmail.com>
2021-02-16 16:54:16 +0900

    add new function

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -18,5 +19,16 @@ def signupfunc(request):
             )
     return render(request, "signup.html", {})
 
+
 def loginfunc(request):
-    
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=username)
+        if user is not None:
+            login(request, user)
+            redirect()
+
+
+def listfunc(request):
+    return render(request, "list.html", {})
